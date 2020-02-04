0f9577e72dad70bf3199a658d001081fce208638
SylvainCatillon <sylvain.catillon@gmail.com>
2020-02-04 11:59:33 +0100

    User message sent to views. Added favicon

--- app/static/js/GrandPy_Bot.js
+++ app/static/js/GrandPy_Bot.js
@@ -1,9 +1,25 @@
 $(function() {
 	$("#user_form").on("submit", function(event) {
-		message = $("#user_message").val();
-		$("#dialog_box").append("<p>" + message + "</p>");
+		var user_message = $("#user_message").val();
 		event.preventDefault();
 		event.stopPropagation();
+		var request = new XMLHttpRequest();
+		var url = window.location.href + "getResponse?user_message=" + user_message;
+		request.open("GET", url);
+		request.send();
+//		jQuerry.get("https://fr.wikipedia.org/w/api.php?action=query&list=geosearch&gscoord=48.8748465|2.3504873&gsradius=10000&gslimit=10");
+//		$.ajax({
+//			url: window.location.hostname + "/getResponse",
+//			data: {
+//				user_message: "test"
+//			},
+//			type: "GET",
+//			dataType: "json",
+//		}).done(function(json) {
+//			alert("done");
+//		}).fail(function(xhr, status, errorThrown) {
+//			alert("Sorry, there was a problem in the request");
+//		});
 		$('#user_form').trigger("reset");
 	})
 })
--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route("/getResponse")
+def getResponse():
+    user_message = request.args.get("user_message")
+    print(user_message)
+    print(type(user_message))
+    return {"message": "test"}
 
 if __name__ == "__main__":
     app.run()
