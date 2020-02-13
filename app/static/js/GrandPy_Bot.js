e110dfec09494df6cf8a77ea2540566da7297822
SylvainCatillon <sylvain.catillon@gmail.com>
2020-02-13 13:32:12 +0100

    Added a delay before displaying the story

--- app/static/js/GrandPy_Bot.js
+++ app/static/js/GrandPy_Bot.js
@@ -1,5 +1,5 @@
 $(function() {
-	var loading = $('<div class="d-flex justify-content-center"><div class="spinner-border text-info" role="status"><span class="sr-only">Loading...</span></div></div>')
+	var loading = $('<div class="d-flex justify-content-center"><div class="spinner-border text-info" role="status"><span class="sr-only">Loading...</span></div></div>');
 
 	$("#user_form").on("submit", function(event) {
 		var user_message = $("#user_message").val();
@@ -9,30 +9,30 @@ $(function() {
 			var dialog_box = $("#dialog_box");
 			$("<p></p>", {"class": "alert alert-info mx-3 rounded shadow-lg", text: user_message}).appendTo(dialog_box);
 			var request = new XMLHttpRequest();
-			request.onreadystatechange = function() {
+			request.onreadystatechange = async function() {
 				if (this.readyState == XMLHttpRequest.DONE) {
 					loading.remove()
 					if (this.status == 200) {
 						var result = JSON.parse(this.responseText);
 						if (result.status == "OK") {
 							$("<p></p>", {"class": "alert alert-success mx-3 rounded shadow-lg", text: result.address}).appendTo(dialog_box)
-							var map = $("<iframe></iframe>", {"class": "mx-auto my-3 d-block", id: "map", frameborder: 0, style: "border: 0; width: 95%; height: 45%;", allowfullscreen: 1, src: result.map_url, text: "carte du lieu"})
-							map.on("load", function() {
-								dialog_box.scrollTop(dialog_box[0].scrollHeight);
-								})
+							var map = $("<iframe></iframe>", {"class": "mx-auto my-3 d-block", id: "map", frameborder: 0, style: "border: 0; width: 95%; height: 45%;", allowfullscreen: 1, src: result.map_url, text: "carte du lieu"});
 							map.appendTo(dialog_box)
+							dialog_box.scrollTop(dialog_box[0].scrollHeight);
+							await new Promise(r => setTimeout(r, 3000));
 							//attention texte dans js, à eviter!!
-							$("<p></p>", {"class": "alert alert-success mx-3 rounded shadow-lg", text: result.story}).append($("<a></a>", {href: result.story_url, text: result.story_link_text})).appendTo(dialog_box)
+							$("<p></p>", {"class": "alert alert-success mx-3 rounded shadow-lg", text: result.story}).append($("<a></a>", {href: result.story_url, text: result.story_link_text})).appendTo(dialog_box);
 						} else if (result.status == "address_not_found") {
-							$("<p></p>", {"class": "alert alert-warning mx-3 rounded shadow-lg", text: result.message}).appendTo(dialog_box)
+							$("<p></p>", {"class": "alert alert-warning mx-3 rounded shadow-lg", text: result.message}).appendTo(dialog_box);
 						}
 					} else {
 						// attention, message dans js, à eviter
-						$("<p></p>", {"class": "alert alert-danger mx-3 rounded shadow-lg", text: "Houla, je crois que ma connexion ne marche pas bien.. Ca doit etre l'age!!"}).appendTo(dialog_box)
+						$("<p></p>", {"class": "alert alert-danger mx-3 rounded shadow-lg", text: "Houla, je crois que ma connexion ne marche pas bien.. Ca doit etre l'age!!"}).appendTo(dialog_box);
 					}
 				} else {
 					dialog_box.append(loading);
-				} dialog_box.scrollTop(dialog_box[0].scrollHeight);
+				}
+				dialog_box.scrollTop(dialog_box[0].scrollHeight);
 			}
 			var url = window.location.origin + "/get_response?user_message=" + user_message;
 			request.open("GET", url);
