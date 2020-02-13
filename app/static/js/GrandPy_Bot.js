21aa83921f9afc6c7c69421d71c6ffa7b3c9a5a2
SylvainCatillon <sylvain.catillon@gmail.com>
2020-02-13 16:34:20 +0100

    Clarified GrandPy_Bot.js

--- app/static/js/GrandPy_Bot.js
+++ app/static/js/GrandPy_Bot.js
@@ -1,43 +1,90 @@
 $(function() {
-	var loading = $('<div class="d-flex justify-content-center"><div class="spinner-border text-info" role="status"><span class="sr-only">Loading...</span></div></div>');
 
-	$("#user_form").on("submit", function(event) {
+	// Create a loading animation
+	var loading =
+		$('<div class="d-flex justify-content-center">\
+			<div class="spinner-border text-info" role="status">\
+			<span class="sr-only">Loading...</span></div></div>');
+
+	var dialog_box = $("#dialog_box");
+
+	async function showAnswer() {
+		if (this.readyState == XMLHttpRequest.DONE) {
+			loading.remove();
+			if (this.status == 200) {
+				var result = JSON.parse(this.responseText);
+				if (result.status == "OK") {
+					// Display the address
+					$("<p></p>", {
+						"class": "alert alert-success mx-3 rounded shadow-lg",
+						text: result.address
+					}).appendTo(dialog_box);
+
+					// Display the map
+					$("<iframe></iframe>", {
+						"class": "mx-auto my-3 d-block",
+						id: "map", frameborder: 0,
+						style: "border: 0; width: 95%; height: 45%;",
+						allowfullscreen: 1, src: result.map_url,
+						text: "carte du lieu"
+					}).appendTo(dialog_box);
+
+					dialog_box.scrollTop(dialog_box[0].scrollHeight);
+
+					// Await 3 sec before displaying the story
+					await new Promise(r => setTimeout(r, 3000));
+					$("<p></p>", {
+						"class": "alert alert-success mx-3 rounded shadow-lg",
+						text: result.story
+					}).append($("<a></a>", {
+						href: result.story_url,
+						text: result.story_link_text
+					})).appendTo(dialog_box);
+
+				} else if (result.status == "address_not_found") {
+					$("<p></p>", {
+						"class": "alert alert-warning mx-3 rounded shadow-lg",
+						text: result.message
+					}).appendTo(dialog_box);
+				}
+			} else {
+				// attention, message dans js, à eviter
+				$("<p></p>", {
+					"class": "alert alert-danger mx-3 rounded shadow-lg",
+					text: "Houla, je crois que ma connexion ne marche pas\
+					 bien.. Ca doit etre l'age!!"
+				}).appendTo(dialog_box);
+			}
+		} else {
+			dialog_box.append(loading);
+		}
+		dialog_box.scrollTop(dialog_box[0].scrollHeight);
+	}
+
+	function askQuestion(event) {
 		var user_message = $("#user_message").val();
 		event.preventDefault();
 		event.stopPropagation();
 		if (user_message) {
-			var dialog_box = $("#dialog_box");
-			$("<p></p>", {"class": "alert alert-info mx-3 rounded shadow-lg", text: user_message}).appendTo(dialog_box);
+			// Display the question of the user
+			$("<p></p>", {
+				"class": "alert alert-info mx-3 rounded shadow-lg",
+				text: user_message
+			}).appendTo(dialog_box);
+
+			// Send the question to the back-end
 			var request = new XMLHttpRequest();
-			request.onreadystatechange = async function() {
-				if (this.readyState == XMLHttpRequest.DONE) {
-					loading.remove()
-					if (this.status == 200) {
-						var result = JSON.parse(this.responseText);
-						if (result.status == "OK") {
-							$("<p></p>", {"class": "alert alert-success mx-3 rounded shadow-lg", text: result.address}).appendTo(dialog_box)
-							var map = $("<iframe></iframe>", {"class": "mx-auto my-3 d-block", id: "map", frameborder: 0, style: "border: 0; width: 95%; height: 45%;", allowfullscreen: 1, src: result.map_url, text: "carte du lieu"});
-							map.appendTo(dialog_box)
-							dialog_box.scrollTop(dialog_box[0].scrollHeight);
-							await new Promise(r => setTimeout(r, 3000));
-							//attention texte dans js, à eviter!!
-							$("<p></p>", {"class": "alert alert-success mx-3 rounded shadow-lg", text: result.story}).append($("<a></a>", {href: result.story_url, text: result.story_link_text})).appendTo(dialog_box);
-						} else if (result.status == "address_not_found") {
-							$("<p></p>", {"class": "alert alert-warning mx-3 rounded shadow-lg", text: result.message}).appendTo(dialog_box);
-						}
-					} else {
-						// attention, message dans js, à eviter
-						$("<p></p>", {"class": "alert alert-danger mx-3 rounded shadow-lg", text: "Houla, je crois que ma connexion ne marche pas bien.. Ca doit etre l'age!!"}).appendTo(dialog_box);
-					}
-				} else {
-					dialog_box.append(loading);
-				}
-				dialog_box.scrollTop(dialog_box[0].scrollHeight);
-			}
-			var url = window.location.origin + "/get_response?user_message=" + user_message;
+			request.onreadystatechange = showAnswer;
+			var url =
+				window.location.origin +
+				"/get_response?user_message=" +
+				user_message;
+
 			request.open("GET", url);
 			request.send();
 			$('#user_form').trigger("reset");
 		}
-	})
-})
+	}
+
+	$("#user_form").on("submit", askQuestion);
+});
