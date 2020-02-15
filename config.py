fdd66b9a0567222747f3ddc3926d1ee074d827c9
SylvainCatillon <sylvain.catillon@gmail.com>
2020-02-15 17:59:52 +0100

    Updated config.py to get GOOGLE_API_KEY from the environment

--- config.py
+++ config.py
@@ -1,19 +1,28 @@
+import os
+
 import text
 from app.stopwords_fr import STOPWORDS_LIST
 
+#  Try to import instance config if it exists
 try:
     from instance import config as instance_config
 except ImportError:
     instance_config = None
 
 
+#  Get GOOGLE_API_KEY from instance config if it exists
 if instance_config:
     GOOGLE_API_KEY = instance_config.GOOGLE_API_KEY
+#  Get GOOGLE_API_KEY from environment, with an empty string as default
 else:
-    GOOGLE_API_KEY = ""
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
 
+#  The sentences to be used in the program
 TEXT = text.fr_text
 
+#  The starting and ending keywords to find an address in the user question
 START_KEYWORDS = ["adresse", "où", "l'adresse"]
 END_KEYWORDS = ["?", ".", "!", ";"]
+
+#  The words to delete from the user question
 STOPWORDS = STOPWORDS_LIST+["grandpy", "bot", "grandpybot"]
